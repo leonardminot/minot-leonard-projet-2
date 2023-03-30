4b3b4ea484b3fd3ec25c973c06d5400be83c3e83
Léonard MINOT <leonard.minot@gadz.org>
2023-03-30 15:08:20 +0200

    Suppression of main method from AnalyticsCounter and useless variables

--- com/hemebiotech/analytics/AnalyticsCounter.java
+++ com/hemebiotech/analytics/AnalyticsCounter.java
@@ -9,9 +9,6 @@ import java.util.Map;
 import java.util.TreeMap;
 
 public class AnalyticsCounter {
-	private static int headacheCount = 0;
-	private static int rashCount = 0;
-	private static int pupilCount = 0;
 
 	private final ISymptomReader symptomReader;
 	private final ISymptomWriter symptomWriter;
@@ -41,37 +38,5 @@ public class AnalyticsCounter {
 
 	public void writeSymptoms(Map<String, Integer> symptoms) {
 		symptomWriter.writeSymptoms(symptoms);
-	}
-	
-	public static void main(String args[]) throws Exception {
-		// first get input
-		BufferedReader reader = new BufferedReader (new FileReader("symptoms.txt"));
-		String line = reader.readLine();
-
-		int i = 0;
-		int headCount = 0;
-		while (line != null) {
-			i++;
-			System.out.println("symptom from file: " + line);
-			if (line.equals("headache")) {
-				headCount++;
-				System.out.println("number of headaches: " + headCount);
-			}
-			else if (line.equals("rush")) {
-				rashCount++;
-			}
-			else if (line.contains("pupils")) {
-				pupilCount++;
-			}
-
-			line = reader.readLine();	// get another symptom
-		}
-		
-		// next generate output
-		FileWriter writer = new FileWriter ("result.out");
-		writer.write("headache: " + headacheCount + "\n");
-		writer.write("rash: " + rashCount + "\n");
-		writer.write("dialated pupils: " + pupilCount + "\n");
-		writer.close();
 	}
 }
