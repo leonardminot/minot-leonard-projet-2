d0fa75f7057702ff223c3f72102f2419a82051d2
Léonard MINOT <leonard.minot@gadz.org>
2023-03-30 14:29:38 +0200

    CODIO BUG : BufferedWriter out of the try

--- com/hemebiotech/analytics/WriteSymptomDataToFile.java
+++ com/hemebiotech/analytics/WriteSymptomDataToFile.java
@@ -10,7 +10,8 @@ public class WriteSymptomDataToFile implements ISymptomWriter {
     private final static String FILE_PATH = "result.out";
     public void writeSymptoms(Map<String, Integer> symptoms) {
 
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter(FILE_PATH, false))) {
+        try {
+            BufferedWriter writer = new BufferedWriter(new FileWriter(FILE_PATH, false));
             for (Map.Entry<String, Integer> entry : symptoms.entrySet()) {
                 writer.write(entry.getKey() + ": " + entry.getValue());
                 writer.newLine();
