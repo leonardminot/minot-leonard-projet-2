dd78816a0b65e0465e447334e8b7b3406298ed81
Léonard MINOT <leonard.minot@gadz.org>
2023-03-30 14:17:16 +0200

    refacto WriteSymptomDataToFile for writer.close()

--- com/hemebiotech/analytics/WriteSymptomDataToFile.java
+++ com/hemebiotech/analytics/WriteSymptomDataToFile.java
@@ -3,26 +3,18 @@ package com.hemebiotech.analytics;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.Iterator;
 import java.util.Map;
 
 public class WriteSymptomDataToFile implements ISymptomWriter {
 
-    public WriteSymptomDataToFile() {
-
-    }
-
-    @Override
+    private final static String FILE_PATH = "result.out";
     public void writeSymptoms(Map<String, Integer> symptoms) {
-        try {
-            BufferedWriter writer = new BufferedWriter(new FileWriter("result.out", false));
 
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(FILE_PATH, false))) {
             for (Map.Entry<String, Integer> entry : symptoms.entrySet()) {
                 writer.write(entry.getKey() + ": " + entry.getValue());
                 writer.newLine();
             }
-
-            //writer.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
