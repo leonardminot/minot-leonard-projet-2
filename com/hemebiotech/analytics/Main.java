7a634cbc2b02b24b7063bdd6727ed533a9c52236
Léonard MINOT <leonard.minot@gadz.org>
2023-03-30 14:56:55 +0200

    add Main class

--- com/hemebiotech/analytics/Main.java
+++ com/hemebiotech/analytics/Main.java
@@ -3,4 +3,17 @@ package com.hemebiotech.analytics;
 import java.util.List;
 import java.util.Map;
 
-// WRITE YOUR CODE HERE
+public class Main {
+    public static void main(String[] args) {
+        ISymptomReader reader = new ReadSymptomDataFromFile("symptoms.txt");
+        ISymptomWriter writer = new WriteSymptomDataToFile();
+        AnalyticsCounter analyticsCounter = new AnalyticsCounter(reader, writer);
+
+        List<String> listOfSymptoms = analyticsCounter.getSymptoms();
+        Map<String, Integer> symptomsOccurence = analyticsCounter.countSymptoms(listOfSymptoms);
+
+        symptomsOccurence = analyticsCounter.sortSymptoms(symptomsOccurence);
+
+        analyticsCounter.writeSymptoms(symptomsOccurence);
+    }
+}
