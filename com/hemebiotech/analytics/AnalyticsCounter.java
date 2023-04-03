a13498c10def18d8cf7380752cac795fb7f34820
Léonard MINOT <leonard.minot@gadz.org>
2023-04-03 11:47:06 +0200

    Remove unused import

--- com/hemebiotech/analytics/AnalyticsCounter.java
+++ com/hemebiotech/analytics/AnalyticsCounter.java
@@ -1,9 +1,5 @@
 package com.hemebiotech.analytics;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
