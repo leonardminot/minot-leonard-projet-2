e0747f17d2d71a2c777fb4acc994907b2170ed97
Léonard MINOT <leonard.minot@gadz.org>
2023-04-03 11:45:48 +0200

    Add basic documentation for interfaces

--- com/hemebiotech/analytics/ISymptomWriter.java
+++ com/hemebiotech/analytics/ISymptomWriter.java
@@ -4,13 +4,14 @@ import java.util.Map;
 
 
 /**
- * Anything the will write symptom data on a valid data output format
+ * Anything that will write symptom data from a Map to a file.
  *
  */
 public interface ISymptomWriter {
     /**
+     * Write a Map of symptoms into an external file
      *
-     * @param symptoms
+     * @param symptoms Map of all the symptoms as keys and the occurrence as value
      */
     void writeSymptoms(Map<String, Integer> symptoms);
 }
